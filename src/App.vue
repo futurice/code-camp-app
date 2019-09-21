25ef9334fc40fc86c6457386716f936a7c36ad1a
Jonathan Sarry <jonathan.sarry@futurice.com>
2019-09-21 14:10:17 +0300

    logo splash

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,55 @@
 <template>
-  <div id="app">
-    <Webcam />
-    <div class="container">
-      <Indicator class="bounce" />
+    <div id="app">
+        <div v-show="loading" class="splash">
+            <SplashScreen />
+        </div>
+        <div v-show="!loading" class="main">
+            <Webcam/>
+            <div class="container">
+                <Indicator class="bounce"/>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import Webcam from "./components/Webcam.vue";
-import Indicator from "./components/Indicator.vue";
+    import Webcam from "./components/Webcam.vue";
+    import Indicator from "./components/Indicator.vue";
+    import SplashScreen from  "./components/SplashScreen"
 
-export default {
-  name: "app",
-  components: {
-    Webcam,
-    Indicator
-  }
-};
+    export default {
+        name: "app",
+        data() {
+            return {
+                loading: true,
+            }
+        },
+        beforeCreate() {
+            setTimeout( () => {
+                this.loading = false
+            }, 3000)
+        },
+        components: {
+            Webcam,
+            Indicator,
+            SplashScreen
+        }
+    };
 </script>
 
 <style scoped>
-#app {
-  position: relative;
-}
-.container {
-  position: absolute;
-  height: 100%;
-  width: 100%;
-  bottom: 0;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-end;
-}
+    .main {
+        position: relative;
+    }
+
+    .container {
+        position: absolute;
+        height: 100%;
+        width: 100%;
+        bottom: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: flex-end;
+    }
 </style>
