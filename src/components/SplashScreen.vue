e797272199bb4f73c771fc4d02ad610476c913a6
Jonathan Sarry <jonathan.sarry@futurice.com>
2019-09-21 13:15:19 +0300

    Auto stash before merge of "master" and "origin/master"

---
+++ src/components/SplashScreen.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <div class="logo">
+            <img src="../assets/Logo%20no%20eyes.svg" alt="">
+            <img src="../assets/lefteye.svg" alt="">
+            <img src="../assets/righteye.svg" alt="">
+        </div>
+        <div class="tag-line">
+            <div class="app-name">Mug Factor</div>
+            <div class="loading">Loading...</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SplashScreen"
+    }
+</script>
+
+<style scoped>
+    .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+</style>
