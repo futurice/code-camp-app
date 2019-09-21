4dcfd5e7bab67c4a093411f591539bf1cf07e491
Joonas Sarapalo <joonas.sarapalo@futurice.com>
2019-09-21 11:37:56 +0300

    python code

--- drowsiness_detect.py
+++ drowsiness_detect.py
@@ -5,26 +5,19 @@ calculations. Uses webcam video feed as input.'''
 from scipy.spatial import distance
 from imutils import face_utils
 import numpy as np
-import pygame #For playing sound
 import time
 import dlib
 import cv2
-
-#Initialize Pygame and load music
-pygame.mixer.init()
-pygame.mixer.music.load('audio/alert.wav')
+import os
 
 #Minimum threshold of eye aspect ratio below which alarm is triggerd
 EYE_ASPECT_RATIO_THRESHOLD = 0.3
 
 #Minimum consecutive frames for which eye ratio is below threshold for alarm to be triggered
-EYE_ASPECT_RATIO_CONSEC_FRAMES = 50
+EYE_ASPECT_RATIO_CONSEC_FRAMES = 5
 
-#COunts no. of consecutuve frames below threshold value
-COUNTER = 0
-
-#Load face cascade which will be used to draw a rectangle around detected faces.
-face_cascade = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml")
+#How many images to analyze before returning not drowsy status
+NUM_OF_IMAGES_IN_ONE_ANALYSIS = 10
 
 #This function calculates and return eye aspect ratio
 def eye_aspect_ratio(eye):
@@ -43,66 +36,87 @@ predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS['left_eye']
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS['right_eye']
 
-#Start webcam video capture
-video_capture = cv2.VideoCapture(0)
+def analyze_picture(state):
+    #Open the next image file
+    frame = cv2.imread(state['image_name'])
 
-#Give some time for camera to initialize(not required)
-time.sleep(2)
-
-while(True):
     #Read each frame and flip it, and convert to grayscale
-    ret, frame = video_capture.read()
-    frame = cv2.flip(frame,1)
+    frame = cv2.flip(frame, 1)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     #Detect facial points through detector function
     faces = detector(gray, 0)
 
-    #Detect faces through haarcascade_frontalface_default.xml
-    face_rectangle = face_cascade.detectMultiScale(gray, 1.3, 5)
-
-    #Draw rectangle around each face detected
-    for (x,y,w,h) in face_rectangle:
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-
     #Detect facial points
     for face in faces:
-
-        shape = predictor(gray, face)
-        shape = face_utils.shape_to_np(shape)
-
-        #Get array of coordinates of leftEye and rightEye
-        leftEye = shape[lStart:lEnd]
-        rightEye = shape[rStart:rEnd]
-
-        #Calculate aspect ratio of both eyes
-        leftEyeAspectRatio = eye_aspect_ratio(leftEye)
-        rightEyeAspectRatio = eye_aspect_ratio(rightEye)
-
-        eyeAspectRatio = (leftEyeAspectRatio + rightEyeAspectRatio) / 2
-
-        #Use hull to remove convex contour discrepencies and draw eye shape around eyes
-        leftEyeHull = cv2.convexHull(leftEye)
-        rightEyeHull = cv2.convexHull(rightEye)
-        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+        eye_aspect_ratio = get_eye_aspect_ratio(gray, face)
 
         #Detect if eye aspect ratio is less than threshold
-        if(eyeAspectRatio < EYE_ASPECT_RATIO_THRESHOLD):
-            COUNTER += 1
-            #If no. of frames is greater than threshold frames,
-            if COUNTER >= EYE_ASPECT_RATIO_CONSEC_FRAMES:
-                pygame.mixer.music.play(-1)
-                cv2.putText(frame, "You are Drowsy", (150,200), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,0,255), 2)
-        else:
-            pygame.mixer.music.stop()
-            COUNTER = 0
+        if (eye_aspect_ratio < EYE_ASPECT_RATIO_THRESHOLD):
+            if state['drowsiness_count'] >= EYE_ASPECT_RATIO_CONSEC_FRAMES:
+                return (state['drowsiness_count'] + 1, state['max_drowsiness'] + 1)
+            else:
+                return (state['drowsiness_count'] + 1, state['max_drowsiness'])
 
-    #Show video feed
-    cv2.imshow('Video', frame)
-    if(cv2.waitKey(1) & 0xFF == ord('q')):
-        break
+    return (0, state['max_drowsiness'])
 
-#Finally when video capture is over, release the video capture and destroyAllWindows
-video_capture.release()
-cv2.destroyAllWindows()
+def get_eye_aspect_ratio(gray, face):
+    shape = predictor(gray, face)
+    shape = face_utils.shape_to_np(shape)
+
+    #Get array of coordinates of leftEye and rightEye
+    leftEye = shape[lStart:lEnd]
+    rightEye = shape[rStart:rEnd]
+
+    #Calculate aspect ratio of both eyes
+    leftEyeAspectRatio = eye_aspect_ratio(leftEye)
+    rightEyeAspectRatio = eye_aspect_ratio(rightEye)
+
+    eyeAspectRatio = (leftEyeAspectRatio + rightEyeAspectRatio) / 2
+
+    return eyeAspectRatio
+
+image_name_generator = (f"./images/image_{seq}.jpg" for seq in range(10000000000))
+
+# running drowsiness counter, image counter, highest drowsiness level yet, current image
+# 0 = not drowsy
+# 1 = drowsy
+
+def fresh_analyzer_state():
+    return {
+        'drowsiness_count': 0, 
+        'image_count': 0,
+        'max_drowsiness': 0,
+        'image_name': ''
+    }
+
+analyzer_state = fresh_analyzer_state()
+
+while True:
+    for image_name in image_name_generator:
+        print(image_name)
+        # loop until next image in the sequence is found 
+        while True:
+            if (os.path.exists(image_name) and os.path.isfile(image_name)):
+                analyzer_state['image_count'] += 1 
+                analyzer_state['image_name'] = image_name
+                drowsiness_count, max_drowsiness = analyze_picture(analyzer_state)
+                analyzer_state['drowsiness_count'] = drowsiness_count
+
+                if (max_drowsiness != 0):
+                    if (analyzer_state['max_drowsiness'] < max_drowsiness):
+                        analyzer_state['max_drowsiness'] = max_drowsiness
+                        with open('./images/result.txt', 'w') as f:
+                            print("T")
+                            f.write("t")
+                # subject was not drowsy
+                elif (analyzer_state['image_count'] == NUM_OF_IMAGES_IN_ONE_ANALYSIS):
+                    if (analyzer_state['max_drowsiness'] == 0):
+                        with open('./images/result.txt', 'w') as f:
+                            print("F")
+                            f.write("f")
+                    analyzer_state = fresh_analyzer_state()
+
+                print("remove...")
+                #os.remove(image_name)
+                break
